11247dc560c36cee7b7a0098b6e4a1e2c35f0e4b
genia <Deiting1AeHae1av>
2019-02-05 13:23:40 +0200

    point check

--- app/addons/sendpulse/controllers/backend/sendpulse_push.php
+++ app/addons/sendpulse/controllers/backend/sendpulse_push.php
@@ -30,10 +30,9 @@ if ($mode == 'init'){
             exit();
         }
 
-        fn_set_notification('K', __('sp.name'), fn_url(''));
+        $push_js_url = $sendpulse_client->getPushJsUrl( fn_url('') );
+        fn_set_notification('K', __('sp.name'), $push_js_url);
         exit();
-
-//        $push_js_url = $sendpulse_client->getPushJsUrl( fn_url('') );
 //
 //        if( $push_js_url==null )
 //        {
